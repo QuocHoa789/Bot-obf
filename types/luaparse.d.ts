967e285fd588d321ee34c40958e7342e831b327b
QuocHoa789 <quochoapham47@gmail.com>
2026-07-07 23:03:49 +0700

    Delete types directory

--- types/luaparse.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'luaparse' {
-  interface Options {
-    comments?: boolean;
-    scope?: boolean;
-    locations?: boolean;
-    ranges?: boolean;
-  }
-  export function parse(code: string, options?: Options): any;
-}
