a4ee78516e7ac9a5108590a7b52180b46bd08376
QuocHoa789 <quochoapham47@gmail.com>
2026-07-07 23:03:35 +0700

    Delete src directory

--- src/utils/random.ts
+++
@@ -1,7 +0,0 @@
-let counter = 0;
-export function randomName(prefix = 'l_'): string {
-  return prefix + (counter++).toString(36) + Math.random().toString(36).substr(2, 5);
-}
-export function randomInt(min: number, max: number): number {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
